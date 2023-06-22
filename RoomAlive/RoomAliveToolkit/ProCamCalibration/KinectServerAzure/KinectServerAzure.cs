046da24935a43781443413390f030f1f5c60648a
P0trak <patricklotery@gmail.com>
2023-06-22 16:45:28 +0100

    Add RoomAlive calibration code

---
+++ RoomAlive/RoomAliveToolkit/ProCamCalibration/KinectServerAzure/KinectServerAzure.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using Microsoft.Azure.Kinect.Sensor;
+using RoomAliveToolkit;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.ServiceModel;
+using System.ServiceModel.Discovery;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using SharpDX.WIC;
+using RoomAliveToolKit;
+
+namespace RoomAliveToolKit
+{
+    public class KinectHandlerAzure
+    {
+        public static KinectHandlerAzure instance;
+        Device kinectSensor;
+
+        //depth stuff
+        public ushort[] depthShortBuffer = new ushort[KinectAzureCalibration.depthImageWidth * KinectAzureCalibration.depthImageHeight];
+        public byte[] depthByteBuffer = new byte[KinectAzureCalibration.depthImageWidth * KinectAzureCalibration.depthImageHeight * 2];
+        public List<AutoResetEvent> depthFrameReady = new List<AutoResetEvent>();
+
+        //colour stuff
+        public byte[] yuvByteBuffer = new byte[KinectAzureCalibration.colorImageWidth * KinectAzureCalibration.colorImageHeight * 2];
+        public List<AutoResetEvent> yuvFrameReady = new List<AutoResetEvent>();
+        public byte[] rgbByteBuffer = new byte[KinectAzureCalibration.colorImageWidth * KinectAzureCalibration.colorImageHeight * 4];
+        public List<AutoResetEvent> rgbFrameReady = new List<AutoResetEvent>();
+        public byte[] jpegByteBuffer = new byte[KinectAzureCalibration.colorImageWidth * KinectAzureCalibration.colorImageHeight * 4];
+        public List<AutoResetEvent> jpegFrameReady = new List<AutoResetEvent>();
+        public int nJpegBytes = 0;
+
+        public float lastColorGain;
+        public long lastColorExposureTimeTicks;
+
+        public KinectAzureCalibration kinectAzureCalibration;
+        public ManualResetEvent kinectAzureCalibrationReady = new ManualResetEvent(false);
+
+        ImagingFactory imagingFactory = new ImagingFactory();
+        Stopwatch stopWatch = new Stopwatch();
+
+        public KinectHandlerAzure()
+        {
+            instance = this;
+            kinectSensor = Device.Open();
+
+            kinectAzureCalibration = new KinectAzureCalibration();
+            kinectAzureCalibration.RecoverCalibrationFromSensor(kinectSensor);
+            kinectAzureCalibrationReady.Set();
+            //kinectSensor.CoordinateMapper.CoordinateMappingChanged += CoordinateMapper_CoordinateMappingChanged;
+
+            Capture capture = kinectSensor.GetCapture();
+            Image colourFrame = capture.Color;
+            Image depthFrame = capture.Depth;
+            processColourFrame(colourFrame);
+            processDepthFrame(depthFrame);
+        }
+
+        //not sure about the coordinate mapper stuff - do i actually need this?
+        // there's also a whole bunch of what seem to be function overrides? like idk how important they are
+
+        void processDepthFrame(Image depthFrame)
+        {
+            if (depthFrame != null)
+            {
+                using (depthFrame)
+                {
+                    if (depthFrameReady.Count > 0)
+                    {
+                        lock (depthShortBuffer)
+                        {
+                            depthFrame.Reference();
+                            Memory<ushort> pixels = depthFrame.GetPixels<ushort>();
+                            pixels.ToArray().CopyTo(depthShortBuffer, 0);
+                            //depthFrame.CopyFrameDataToArray(depthShortBuffer);
+                        }
+                            
+                        lock (depthFrameReady)
+                            foreach (var autoResetEvent in depthFrameReady)
+                                autoResetEvent.Set();
+                    }
+                }
+            }
+        }
+
+        void processColourFrame(Image colourFrame) 
+        {
+            if (colourFrame != null)
+            {
+                using (colourFrame)
+                {
+                    lastColorGain = (float) ColorControlCommand.Gain;
+                    lastColorExposureTimeTicks = colourFrame.SystemTimestampNsec / 100; //1 tick is 100 nanoseconds
+
+                    if (yuvFrameReady.Count > 0)
+                    {
+                        lock (yuvByteBuffer)
+                        {
+                            colourFrame.Reference();
+                            Memory<ushort> pixels = colourFrame.GetPixels<ushort>();
+                            pixels.ToArray().CopyTo(yuvByteBuffer, 0);
+                        }
+                        lock (yuvFrameReady)
+                            foreach (var autoResetEvent in yuvFrameReady)
+                                autoResetEvent.Set();
+                    }
+
+                    if ((rgbFrameReady.Count > 0) || (jpegFrameReady.Count > 0))
+                    {
+                        lock (rgbByteBuffer)
+                        {
+                            Memory<ushort> pixels = colourFrame.GetPixels<ushort>();
+                            pixels.ToArray().CopyTo(yuvByteBuffer, 0);
+                        }
+                            //colourFrame.CopyConvertedFrameDataToArray(rgbByteBuffer, ImageFormat.ColorBGRA32);
+                            // may need to figure out how to do this
+                        lock (rgbFrameReady)
+                            foreach (var autoResetEvent in rgbFrameReady)
+                                autoResetEvent.Set();
+                    }
+
+                    if (jpegFrameReady.Count > 0)
+                    {
+                        // should be put in a separate thread?
+
+                        stopWatch.Restart();
+
+                        var bitmapSource = new Bitmap(imagingFactory, KinectAzureCalibration.colorImageWidth, KinectAzureCalibration.colorImageHeight, SharpDX.WIC.PixelFormat.Format32bppBGR, BitmapCreateCacheOption.CacheOnLoad);
+                        var bitmapLock = bitmapSource.Lock(BitmapLockFlags.Write);
+                        Marshal.Copy(rgbByteBuffer, 0, bitmapLock.Data.DataPointer, KinectAzureCalibration.colorImageWidth * KinectAzureCalibration.colorImageHeight * 4);
+                        bitmapLock.Dispose();
+
+                        var memoryStream = new MemoryStream();
+
+                        //var fileStream = new FileStream("test" + frame++ + ".jpg", FileMode.Create);
+                        //var stream = new WICStream(imagingFactory, "test" + frame++ + ".jpg", SharpDX.IO.NativeFileAccess.Write);
+
+                        var stream = new WICStream(imagingFactory, memoryStream);
+
+                        var jpegBitmapEncoder = new JpegBitmapEncoder(imagingFactory);
+                        jpegBitmapEncoder.Initialize(stream);
+
+                        var bitmapFrameEncode = new BitmapFrameEncode(jpegBitmapEncoder);
+                        bitmapFrameEncode.Options.ImageQuality = 0.5f;
+                        bitmapFrameEncode.Initialize();
+                        bitmapFrameEncode.SetSize(KinectAzureCalibration.colorImageWidth, KinectAzureCalibration.colorImageHeight);
+                        var pixelFormatGuid = PixelFormat.FormatDontCare;
+                        bitmapFrameEncode.SetPixelFormat(ref pixelFormatGuid);
+                        bitmapFrameEncode.WriteSource(bitmapSource);
+
+                        bitmapFrameEncode.Commit();
+                        jpegBitmapEncoder.Commit();
+
+                        //fileStream.Close();
+                        //fileStream.Dispose();
+
+                        //Console.WriteLine(stopWatch.ElapsedMilliseconds + "ms " + memoryStream.Length + " bytes");
+
+                        lock (jpegByteBuffer)
+                        {
+                            nJpegBytes = (int)memoryStream.Length;
+                            memoryStream.Seek(0, SeekOrigin.Begin);
+                            memoryStream.Read(jpegByteBuffer, 0, nJpegBytes);
+                        }
+                        lock (jpegFrameReady)
+                            foreach (var autoResetEvent in jpegFrameReady)
+                                autoResetEvent.Set();
+
+                        //var file = new FileStream("test" + frame++ + ".jpg", FileMode.Create);
+                        //file.Write(jpegByteBuffer, 0, nJpegBytes);
+                        //file.Close();
+
+                        bitmapSource.Dispose();
+                        memoryStream.Close();
+                        memoryStream.Dispose();
+                        stream.Dispose();
+                        jpegBitmapEncoder.Dispose();
+                        bitmapFrameEncode.Dispose();
+                    }
+                }
+            }
+        }
+
+        //idk if we need any of the body frame stuff
+
+    }
+
+    /// <summary>
+    /// Created on each session.
+    /// </summary>
+    [ServiceBehavior(ConcurrencyMode=ConcurrencyMode.Multiple)]
+    [ServiceContract]
+    public class KinectServerAzure
+    {
+        byte[] depthByteBuffer = new byte[KinectAzureCalibration.depthImageWidth * KinectAzureCalibration.depthImageHeight * 2];
+        byte[] yuvByteBuffer = new byte[KinectAzureCalibration.colorImageWidth * KinectAzureCalibration.colorImageHeight * 2];
+        byte[] rgbByteBuffer = new byte[KinectAzureCalibration.colorImageWidth * KinectAzureCalibration.colorImageHeight * 4];
+
+        AutoResetEvent depthFrameReady = new AutoResetEvent(false);
+        AutoResetEvent yuvFrameReady = new AutoResetEvent(false);
+        AutoResetEvent rgbFrameReady = new AutoResetEvent(false);
+        AutoResetEvent jpegFrameReady = new AutoResetEvent(false);
+        //AutoResetEvent audioFrameReady = new AutoResetEvent(false);
+
+        //Queue<byte[]> audioFrameQueue = new Queue<byte[]>();
+
+        public KinectServerAzure()
+        {
+            lock (KinectHandlerAzure.instance.depthFrameReady) // overkill?
+                KinectHandlerAzure.instance.depthFrameReady.Add(depthFrameReady);
+            lock (KinectHandlerAzure.instance.yuvFrameReady)
+                KinectHandlerAzure.instance.yuvFrameReady.Add(yuvFrameReady);
+            lock (KinectHandlerAzure.instance.rgbFrameReady)
+                KinectHandlerAzure.instance.rgbFrameReady.Add(rgbFrameReady);
+            lock (KinectHandlerAzure.instance.jpegFrameReady)
+                KinectHandlerAzure.instance.jpegFrameReady.Add(jpegFrameReady);
+            /*
+            lock (KinectHandlerAzure.instance.audioFrameReady)
+                KinectHandlerAzure.instance.audioFrameReady.Add(audioFrameReady);
+            lock (KinectHandlerAzure.instance.audioFrameQueues)
+                KinectHandlerAzure.instance.audioFrameQueues.Add(audioFrameQueue);
+            */
+
+
+            OperationContext.Current.Channel.Closed += ClientClosed;
+        }
+
+        public void ClientClosed(object sender, EventArgs e)
+        {
+            //Console.WriteLine("ClientClosed");
+
+            // remove ourselves from the singleton
+            lock (KinectHandlerAzure.instance.depthFrameReady) // overkill?
+                KinectHandlerAzure.instance.depthFrameReady.Remove(depthFrameReady);
+            lock (KinectHandlerAzure.instance.yuvFrameReady)
+                KinectHandlerAzure.instance.yuvFrameReady.Remove(yuvFrameReady);
+            lock (KinectHandlerAzure.instance.rgbFrameReady)
+                KinectHandlerAzure.instance.rgbFrameReady.Remove(rgbFrameReady);
+            lock (KinectHandlerAzure.instance.jpegFrameReady)
+                KinectHandlerAzure.instance.jpegFrameReady.Remove(jpegFrameReady);
+            /*
+            lock (KinectHandlerAzure.instance.audioFrameReady)
+                KinectHandlerAzure.instance.audioFrameReady.Remove(audioFrameReady);
+            lock (KinectHandlerAzure.instance.audioFrameQueues)
+                KinectHandlerAzure.instance.audioFrameQueues.Remove(audioFrameQueue);
+            */
+        }
+
+        // Returns immediately if a frame has been made available since the last time this was called on this client;
+        // otherwise blocks until one is available.
+        [OperationContract]
+        public byte[] LatestDepthImage()
+        {
+            depthFrameReady.WaitOne();
+            // Is this copy really necessary?:
+            lock (KinectHandlerAzure.instance.depthShortBuffer)
+                Buffer.BlockCopy(KinectHandlerAzure.instance.depthShortBuffer, 0, depthByteBuffer, 0, KinectAzureCalibration.depthImageWidth * KinectAzureCalibration.depthImageHeight * 2);
+            return depthByteBuffer;
+        }
+
+        [OperationContract]
+        public byte[] LatestYUVImage()
+        {
+            yuvFrameReady.WaitOne();
+            lock (KinectHandlerAzure.instance.yuvByteBuffer)
+                Buffer.BlockCopy(KinectHandlerAzure.instance.yuvByteBuffer, 0, yuvByteBuffer, 0, KinectAzureCalibration.colorImageWidth * KinectAzureCalibration.colorImageHeight * 2);
+            return yuvByteBuffer;
+        }
+
+        [OperationContract]
+        public byte[] LatestRGBImage()
+        {
+            rgbFrameReady.WaitOne();
+            lock (KinectHandlerAzure.instance.rgbByteBuffer)
+                Buffer.BlockCopy(KinectHandlerAzure.instance.rgbByteBuffer, 0, rgbByteBuffer, 0, KinectAzureCalibration.colorImageWidth * KinectAzureCalibration.colorImageHeight * 4);
+            return rgbByteBuffer;
+        }
+
+        [OperationContract]
+        public byte[] LatestJPEGImage()
+        {
+            jpegFrameReady.WaitOne();
+            byte[] jpegByteBuffer;
+            lock (KinectHandlerAzure.instance.jpegByteBuffer)
+            {
+                jpegByteBuffer = new byte[KinectHandlerAzure.instance.nJpegBytes];
+                Buffer.BlockCopy(KinectHandlerAzure.instance.jpegByteBuffer, 0, jpegByteBuffer, 0, KinectHandlerAzure.instance.nJpegBytes);
+            }
+            return jpegByteBuffer;
+        }
+
+        /*
+        [OperationContract]
+        public byte[] LatestAudio()
+        {
+            audioFrameReady.WaitOne();
+            lock (KinectHandler.instance.audioFrameQueues) // overkill?
+            {
+                var buffer = new byte[audioFrameQueue.Count * 1024];
+                int count = audioFrameQueue.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    var thisBuffer = audioFrameQueue.Dequeue();
+                    Array.Copy(thisBuffer, 0, buffer, 1024 * i, 1024);
+                }
+                return buffer;
+            }
+        }
+        */
+
+        [OperationContract]
+        public float LastColorGain()
+        {
+            return KinectHandlerAzure.instance.lastColorGain;
+        }
+
+        [OperationContract]
+        public long LastColorExposureTimeTicks()
+        {
+            return KinectHandlerAzure.instance.lastColorExposureTimeTicks;
+        }
+        [OperationContract]
+        public KinectAzureCalibration GetCalibration()
+        {
+            KinectHandlerAzure.instance.kinectAzureCalibrationReady.WaitOne();
+            return KinectHandlerAzure.instance.kinectAzureCalibration;
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        new KinectHandlerAzure();
+        var serviceHost = new ServiceHost(typeof(KinectServerAzure));
+
+        // discovery
+        serviceHost.Description.Behaviors.Add(new ServiceDiscoveryBehavior());
+        serviceHost.AddServiceEndpoint(new UdpDiscoveryEndpoint());
+
+        Console.WriteLine("hi :)");
+        serviceHost.Open();
+        Console.ReadLine();
+    }
+}
---
+++ RoomAlive/RoomAliveToolkit/README.md
@@ -0,0 +1,57 @@
+# RoomAlive Toolkit README
+
+The RoomAlive Toolkit enables creation of dynamic projection mapping experiences. This toolkit has been in internal use at Microsoft Research for several years and has been used in a variety of interactive projection mapping projects such as [RoomAlive](https://www.youtube.com/watch?v=ILb5ExBzHqw), [IllumiRoom](https://www.youtube.com/watch?v=re1EatGRV0w), [ManoAMano](https://www.youtube.com/watch?v=Df7fZAYVAIE), [Beamatron](https://www.youtube.com/watch?v=Z4bdrG8S1FM) and [Room2Room](https://www.youtube.com/watch?v=tRzOqTRxoek). 
+
+The toolkit consists of two separate projects:
+
+* **[ProCamCalibration](ProCamCalibration/)** - This C# project can be used to calibrate multiple projectors and Kinect cameras in a room to enable immersive, dynamic projection mapping experiences. The codebase also includes a simple projection mapping sample using Direct3D.
+* **[RoomAlive Toolkit for Unity](RoomAliveToolkitForUnity/)** - RoomAlive Toolkit for Unity contains is a set of Unity scripts and tools that enable immersive, dynamic projection mapping experiences, based on the projection-camera calibration from ProCamCalibration. This project also includes a tool to stream and render Kinect depth data to Unity. 
+
+Here is an example scene from our *RoomAlive* project to illustrate what is possible (this one uses 6 projectors and 6 Kinect cameras):
+
+![RoomAlive Scene](RoomAliveToolkitForUnity/docs/images/Roomalive.png?raw=true) 
+
+## Development Status
+
+This project is under development. The current release is in beta and all APIs are subject to change. The next major features might include compute shaders for handling all depth meshes as a single unified vertex buffer, radiometric compensation across dynamic scnenes, support for Unity and Unreal Engines. We welcome contributions!
+
+## Citations
+
+The RoomAlive Project was started in the summer of 2013 with a group of superstar interns. If you are looking for a reference to that original work, please cite: 
+
+```
+@inproceedings{Jones:2014:RME:2642918.2647383,
+ author = {Jones, Brett and Sodhi, Rajinder and Murdock, Michael and Mehra, Ravish and Benko, Hrvoje and Wilson, Andrew and Ofek, Eyal and MacIntyre, Blair and Raghuvanshi, Nikunj and Shapira, Lior},
+ title = {RoomAlive: Magical Experiences Enabled by Scalable, Adaptive Projector-camera Units},
+ booktitle = {Proceedings of the 27th Annual ACM Symposium on User Interface Software and Technology},
+ series = {UIST '14},
+ year = {2014},
+ isbn = {978-1-4503-3069-5},
+ location = {Honolulu, Hawaii, USA},
+ pages = {637--644},
+ numpages = {8},
+ url = {http://doi.acm.org/10.1145/2642918.2647383},
+ doi = {10.1145/2642918.2647383},
+ acmid = {2647383},
+ publisher = {ACM},
+ address = {New York, NY, USA},
+ keywords = {projection mapping, projector-camera system, spatial augmented reality},
+} 
+```
+## Contribute
+
+We welcome contributions to help advance projection mapping research frontier! 
+
+* [File an issue](https://github.com/Microsoft/RoomAliveToolkit/issues) first so we are aware about change you want to make and possibly guide you. Please include these log files when you report an issue.
+* Use [usual steps](https://akrabat.com/the-beginners-guide-to-contributing-to-a-github-project/) to make changes just like other GitHub projects.
+* Clean compile your changes on Windows and test basic operations.
+* When your pull request is created, you might get prompted to one-time sign [Contributor License Agreement (CLA)](https://en.wikipedia.org/wiki/Contributor_License_Agreement) unless changes are minor. It's very simple and takes less than a minute.
+* If your pull request gets a conflict, please resolve it.
+* Watch for any comments on your pull request.
+* Please try and limit your changes to small number of files. We need to review every line of your change and we can't reasonably do that if you make requests with huge number of changes.
+* Do not make just cosmetic changes. We will generally reject pull requests with only cosmetic changes.
+
+## License
+
+This project is licensed under [MIT license](LICENSE). 
+
